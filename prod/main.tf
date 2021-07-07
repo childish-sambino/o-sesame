d4837ba7598d8afc5e7da7bb8c8dd27fdba90eea
Sam Harrison <sharrison@twilio.com>
2021-07-07 12:51:01 -0500

    feat: updated flows

--- prod/main.tf
+++ prod/main.tf
@@ -28,13 +28,14 @@ provider "twilio" {
 resource "twilio_api_accounts_incoming_phone_numbers_v2010" "incoming_number" {
   phone_number = var.incoming_phone_number
   voice_url = "https://webhooks.twilio.com/v1/Accounts/${var.twilio_account_sid}/Flows/${twilio_studio_flows_v2.o_sesame_flow.sid}"
+  sms_url = "https://webhooks.twilio.com/v1/Accounts/${var.twilio_account_sid}/Flows/${twilio_studio_flows_v2.allow_entry_flow.sid}"
 }
 
 resource "twilio_studio_flows_v2" "o_sesame_flow" {
   friendly_name = "O Sesame"
   status = "published"
   definition = jsonencode({
-    description: "A New Flow",
+    description: "O Sesame",
     states: [
       {
         name: "Trigger",
@@ -59,35 +60,20 @@ resource "twilio_studio_flows_v2" "o_sesame_flow" {
         }
       },
       {
-        name: "open_gate",
-        type: "say-play",
-        transitions: [
-          {
-            event: "audioComplete"
-          }
-        ],
-        properties: {
-          offset: {
-            x: 20,
-            y: 740
-          },
-          loop: 2,
-          digits: "9"
-        }
-      },
-      {
         name: "gather_caller",
         type: "gather-input-on-call",
         transitions: [
           {
+            next: "gather_caller",
             event: "keypress"
           },
           {
-            next: "send_message_1",
-            event: "speech"
+            next: "gather_caller",
+            event: "timeout"
           },
           {
-            event: "timeout"
+            next: "run_subflow",
+            event: "speech"
           }
         ],
         properties: {
@@ -108,12 +94,34 @@ resource "twilio_studio_flows_v2" "o_sesame_flow" {
         }
       },
       {
-        name: "send_message_1",
-        type: "send-message",
+        name: "enqueue",
+        type: "enqueue-call",
         transitions: [
           {
-            next: "open_gate",
-            event: "sent"
+            event: "callComplete"
+          },
+          {
+            event: "failedToEnqueue"
+          },
+          {
+            event: "callFailure"
+          }
+        ],
+        properties: {
+          queue_name: "Waiting At Gate",
+          offset: {
+            x: 100,
+            y: 790
+          }
+        }
+      },
+      {
+        name: "run_subflow",
+        type: "make-http-request",
+        transitions: [
+          {
+            next: "enqueue",
+            event: "success"
           },
           {
             event: "failed"
@@ -121,14 +129,144 @@ resource "twilio_studio_flows_v2" "o_sesame_flow" {
         ],
         properties: {
           offset: {
-            x: 220,
-            y: 510
+            x: 160,
+            y: 520
           },
-          service: "{{trigger.message.InstanceSid}}",
-          channel: "{{trigger.message.ChannelSid}}",
+          method: "POST",
+          content_type: "application/x-www-form-urlencoded;charset=utf-8",
+          parameters: [
+            {
+              value: var.outgoing_phone_number,
+              key: "To"
+            },
+            {
+              value: var.incoming_phone_number,
+              key: "From"
+            },
+            {
+              value: "{\"CallerName\":\"{{widgets.gather_caller.SpeechResult}} \", \"CallSid\": \"{{trigger.call.CallSid}}\"}",
+              key: "Parameters"
+            }
+          ],
+          url: "https://${var.twilio_account_sid}:${var.twilio_auth_token}@studio.twilio.com/v2/Flows/${twilio_studio_flows_v2.allow_entry_flow.sid}"
+        }
+      }
+    ],
+    initial_state: "Trigger",
+    flags: {
+      allow_concurrent_calls: true
+    }
+  })
+}
+
+resource "twilio_studio_flows_v2" "allow_entry_flow" {
+  friendly_name = "Allow Entry"
+  status = "published"
+  definition = jsonencode({
+    description: "Allow Entry",
+    states: [
+      {
+        name: "Trigger",
+        type: "trigger",
+        transitions: [
+          {
+            event: "incomingMessage"
+          },
+          {
+            event: "incomingCall"
+          },
+          {
+            next: "send_and_reply",
+            event: "incomingRequest"
+          }
+        ],
+        properties: {
+          offset: {
+            x: 70,
+            y: -110
+          }
+        }
+      },
+      {
+        name: "send_and_reply",
+        type: "send-and-wait-for-reply",
+        transitions: [
+          {
+            next: "test",
+            event: "incomingMessage"
+          },
+          {
+            event: "timeout"
+          },
+          {
+            event: "deliveryFailure"
+          }
+        ],
+        properties: {
+          offset: {
+            x: 180,
+            y: 130
+          },
           from: "{{flow.channel.address}}",
-          to: var.outgoing_phone_number,
-          body: "Hi {{widgets.gather_caller.SpeechResult}}"
+          body: "{{flow.data.CallerName}}",
+          timeout: "30"
+        }
+      },
+      {
+        name: "test",
+        type: "split-based-on",
+        transitions: [
+          {
+            event: "noMatch"
+          },
+          {
+            next: "http_1",
+            event: "match",
+            conditions: [
+              {
+                friendly_name: "If value matches_any_of YES, Yes, yes, Y, y",
+                arguments: [
+                  "{{widgets.send_and_reply.inbound.Body}}"
+                ],
+                type: "matches_any_of",
+                value: "YES, Yes, yes, Y, y"
+              }
+            ]
+          }
+        ],
+        properties: {
+          input: "{{widgets.send_and_reply.inbound.Body}}",
+          offset: {
+            x: 130,
+            y: 400
+          }
+        }
+      },
+      {
+        name: "dial_9",
+        type: "make-http-request",
+        transitions: [
+          {
+            event: "success"
+          },
+          {
+            event: "failed"
+          }
+        ],
+        properties: {
+          offset: {
+            x: 140,
+            y: 690
+          },
+          method: "POST",
+          content_type: "application/x-www-form-urlencoded;charset=utf-8",
+          parameters: [
+            {
+              value: "<Response><Play digits=\"ww9ww\"></Play></Response>",
+              key: "Twiml"
+            }
+          ],
+          url: "https://${var.twilio_account_sid}:${var.twilio_auth_token}@api.twilio.com/2010-04-01/Accounts/${var.twilio_account_sid}/Calls/{{flow.data.CallSid}}.json"
         }
       }
     ],
